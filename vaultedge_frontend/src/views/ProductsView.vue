c992c3e02093459cac8e9642f335ebcddb17052d
1Buhle <buhlesonwabile@gmail.com>
2025-10-05 17:12:39 +0200

    trying something out

--- vaultedge_frontend/src/views/ProductsView.vue
+++ vaultedge_frontend/src/views/ProductsView.vue
@@ -86,7 +86,7 @@ import axios from 'axios';
 const products = ref([]);
 onMounted(async () => {
     try {
-        const response = await axios.get('https://yamanote.proxy.rlwy.net:27729/products');
+        const response = await axios.get('https://localhost:30009/products');
         products.value = response.data;
     } catch (error) {
         console.error('Failed to fetch products:', error);
